9afa65e642413074e412a367c832e802fa7809a5
Gastello <maximgst@gmail.com>
2023-08-09 21:47:23 +0300

    com

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, FC } from "react";
 
-const App = () => {
+const UseState = () => {
   const [state, setState] = useState({ isAdmin: false, userName: "Olga" });
   const { isAdmin, userName } = state;
+
   const assignAdmin = () => {
     setState({ ...state, isAdmin: true });
   };
@@ -13,8 +14,9 @@ const App = () => {
     setState({ ...state, isAdmin: false });
   };
   return (
-    <div className="App">
-      <h1>Hello, {isAdmin ? "Admin" : "User"}</h1>
+    <div>
+      <h1>useState</h1>
+      <h2>Hello, {isAdmin ? "Admin" : "User"}</h2>
       {isAdmin ? (
         <button onClick={assignUser}>Become an user!</button>
       ) : (
@@ -24,4 +26,53 @@ const App = () => {
   );
 };
 
+const UseEffect = () => {
+  const [counter, setCounter] = useState(0);
+  const [counterExists, toggleCounter] = useState(true);
+  type CounterProps = {
+    counter: number;
+    onCount: () => void;
+  };
+  const Counter: FC<CounterProps> = (props) => {
+    return (
+      <button onClick={props.onCount}>Clicked {props.counter} times</button>
+    );
+  };
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+  const resetCounter = () => {
+    setCounter(0);
+    toggleCounter(true);
+  };
+  const hideCounter = () => {
+    toggleCounter(false);
+  }
+  return (
+    <div>
+      <h1>UseEffect</h1>
+      {counterExists && <Counter counter={counter} onCount={incrementCounter} />}
+      <div>
+        <button onClick={resetCounter}>
+          Reset counter
+        </button>
+        <button onClick={hideCounter}>
+          Delete Counter
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <hr />
+      <UseState />
+      <hr />
+      <UseEffect />
+      <hr />
+    </div>
+  );
+};
 export default App;
